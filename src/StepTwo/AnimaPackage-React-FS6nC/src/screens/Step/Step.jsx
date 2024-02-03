d38bc1519949c329e6151b923194a60c22d86f4c
cloudycosmo <yuanamandali2004@gmail.com>
2024-02-03 17:47:56 -0500

    updated text field

--- src/StepTwo/AnimaPackage-React-FS6nC/src/screens/Step/Step.jsx
+++ src/StepTwo/AnimaPackage-React-FS6nC/src/screens/Step/Step.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useState } from 'react';
 import { ButtonV } from "../../components/ButtonV";
 import { StepSymbol } from "../../components/StepSymbol";
 import { StepTextHorizontal } from "../../components/StepTextHorizontal";
 import "./style.css";
 
 export const Prompt = () => {
+  const [prompt, setPrompt] = useState('');
   return (
     <div className="step">
       <div className="div">
@@ -48,7 +50,12 @@ export const Prompt = () => {
               <div className="text-wrapper-2">Your Text</div>
               <p className="p">What do you want to memorize today?</p>
             </div>
-            <div className="rectangle-2" />
+            <div className="rectangle-2" >
+            <textarea
+              onChange={(e) => setPrompt(e.target.value)}
+              placeholder="Enter prompt here" rows="12" style={{ width: '100%', fontSize: '32px' }}>
+            </textarea>
+            </div>
           </div>
           <div className="frame-5">
             <ButtonV className="button-v-1" text="Back" />
